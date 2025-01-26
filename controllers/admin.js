91029a23d570120907afab16bd1f4ff2418fc9f1
Rajath Rao <raorajath1998@gmail.com>
2025-01-26 19:12:50 +0530

    design uthe user and product relationship

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const e = require("express");
 const Product = require("../models/product");
+const { where } = require("sequelize");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
@@ -14,14 +15,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    imageUrl: imageUrl,
-    price: price,
-    description: description,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      imageUrl: imageUrl,
+      price: price,
+      description: description,
+    })
     .then((result) => {
-      console.log("created product", result);
+      // console.log("created product", result);
       res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
@@ -29,12 +31,13 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  console.log(editMode);
+  // console.log(editMode);
   if (!editMode) {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then((product) => {
+  req.user.getProducts({ where: { id: prodId } }).then((products) => {
+    const product = products[0];
     if (!product) {
       return res.redirect("/");
     }
@@ -79,7 +82,7 @@ exports.postEditProduct = (req, res, next) => {
       return product.save();
     })
     .then((result) => {
-      console.log("UPDATED", result);
+      // console.log("UPDATED", result);
       res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
